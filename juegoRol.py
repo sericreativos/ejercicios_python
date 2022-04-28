214df863ab924d98dbdcae0f06b0e3cd11d1d68e
sericreativos <92564730+sericreativos@users.noreply.github.com>
2022-04-28 17:23:57 -0500

    Update juegoRol.py

--- juegoRol.py
+++ juegoRol.py
@@ -11,51 +11,59 @@ atacar, que reciba otro personaje, al que le debe hacer el daño indicado por el
 Escribir una clase Campesino que herede de Personaje, y agregue el atributo cosecha y el método 
 cosechar, que devuelva la cantidad cosechada."""
 
-
-class personaje():
+#Escribir una clase Personaje que contenga los atributos vida, posicion y velocidad,
+class personaje:
     def __init__(self, vida:int, posicion:int, velocidad:int):
-        self.vida= vida
-        print(self.vida)
+        self.vida = vida
         self.posicion = posicion
-        print(self.posicion)
         self.velocidad = velocidad
-        print(self.velocidad)
-
-
-    def recibir_ataque (recibirAtaque, vidaMenos):
+                   
+# los métodos
+#recibir_ataque, que reduzca la vida según una cantidad recibida y
+#lance una excepción si la vida pasa a ser menor o igual que cero
+    def recibir_ataque (self, vida:int, recibirAtaque:int):
         try:
-            recibirAtaque.vida=vidaMenos
-            vidaActual=recibirAtaque-vidaMenos
-            
+            self.vida=vida
+            self.vida -= recibirAtaque
         except:
-            if vidaActual <= 0:
+            if self.vida < 0:
+                self.vida=0
                 print("You loose!!")
+   
 
-    def mover_direccion (moverDireccion, moverCantidad, posicion, velocidad ):
-        moverDireccion.posicion=posicion
-        print(moverDireccion.posicion)
-        moverCantidad.velocidad=velocidad
-        print(moverCantidad)
+#y mover que reciba una dirección 
+#y se mueva en esa dirección la cantidad indicada por velocidad.
+    def mover_direccion (self, direccion:str, velocidad:int ):
+        self.direccion=direccion
+        direccion='derecha'
+        self.velocidad=velocidad
+        velocidad=9
 
-class soldado(personaje):
-    def __init__ (self, vida:int, posicion:int, velocidad:int, ataque:int):
-        personaje.__init__(self, vida, posicion, velocidad, ataque)
 
-    def atacar(personaje_2):
-        def __init__(ataque):
-            soldado.__init__(ataque)
+
+
+#Escribir una clase Soldado que herede de Personaje, y agregue el atributo ataque y el método 
+#atacar, que reciba otro personaje, al que le debe hacer el daño indicado por el atributo ataque.
+
+class soldado:
+    def __init__ (self, vida:int, posicion:int, velocidad:int, ataque:int, cantidadAtaque:int):
+        super(personaje).__init__(self, vida, posicion, velocidad, ataque)
         
-            
+    def atacar(personaje2, self, vida, cantidadAtaque): 
+        self.vida -= vida
+        vida -= cantidadAtaque
+        
+#Escribir una clase Campesino que herede de Personaje, y agregue el atributo cosecha y el método 
+#cosechar, que devuelva la cantidad cosechada.
+class campesino:
+    def __init__(self,vida:int, posicion:int, velocidad:int, cosecha: int, cantidadCosechada:int):
+        super(personaje).__init__(self, vida, posicion, velocidad, cosecha)
 
-class campesino():
-    def __init__(self,vida:int, posicion:int, velocidad:int, cosecha: int):
-        personaje.__init__(self, vida, posicion, velocidad, cosecha)
-
-    def cosechar(self, cosecha):
+    def cosechar(self, cosecha,cantidadCosechada):
         self.cosecha= cosecha
-        return cosecha
-
-    
+        for i in range(cosecha):
+            if cosecha > 1:
+                cantidadCosechada += 1
 
 
 
